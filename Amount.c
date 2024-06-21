76b219c2c3fdbafa5210287aedd0d0c5afc67d5c
Iamamit9 <akumarsingh330@gmail.com>
2024-06-21 16:47:30 +0530

    balance

---
+++ Amount.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void balance();
+int main()
+{
+ balance();
+ return 0;
+}
+void balance()
+{
+    int amount;
+    while (1)
+    {
+        
+        printf("Please Enter Balance (Opening Amount would be 500 mininum) ");
+        scanf("%d", &amount);
+        if (amount >= 500)
+        {
+            break;
+        }
+    }   
+}
+
