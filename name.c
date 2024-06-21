12dfb1a70c1932870de0f0ba29ba299d6aca4092
mazda01 <khatoonmazda94@email.com>
2024-06-21 16:48:21 +0530

    name

---
+++ name.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char Name[50];
+    printf("Please Enter Account Holder Name : ");
+    scanf("%s",&Name);
+    printf("Acccount Holder Name Is : %s",Name);
+
+
+return 0;
