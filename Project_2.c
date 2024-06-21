a1e6e8567f38c3be37081608c6b76550f86fdca9
Anand159 <anandkumarsoni154@gmail.com>
2024-06-21 16:46:34 +0530

    int main

--- Project_2.c
+++ Project_2.c
@@ -1,5 +1,46 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+
 int main()
 {
-    printf("Anand Soni");
+    int TaskNumber;
+
+    printf("\n*******Project 2 - Banking Management System**********");
+    printf("\n1- Create Account");
+    printf("\n2- Deposit");
+    printf("\n3- Withdraw");
+    printf("\n4- check  balance");
+    printf("\n0- Exit");
+
+    while (1)
+    {
+
+        printf("\nPlease enter the task number : ");
+        scanf("%d", &TaskNumber);
+
+        if (TaskNumber == 1)
+        {
+            
+        }
+        else if (TaskNumber == 2)
+        {
+        }
+        else if (TaskNumber == 3)
+        {
+        }
+        else if (TaskNumber == 4)
+        {
+        }
+        else if (TaskNumber == 0)
+        {
+            break;
+        }
+        else
+        {
+            printf("\nInvalid TaskNumber");
+        }
+    }
+
+    return 0;
 }
+
