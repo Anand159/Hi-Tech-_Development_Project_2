0f426d1d24f5f091c6031bd38314ba8ed451eee4
= <=>
2024-06-21 16:42:33 +0530

    name info

--- Project_2.c
+++ Project_2.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
+void name();
 int main()
 {
-    printf("Anand Soni");
+    name();
+    return 0;
+}
+void name()
+{   
+    char name[10];
+    printf("Please enter Account holder name : ");
+    scanf("%s",name);
+    printf("Account holder's name is %s",name);
+
 }
