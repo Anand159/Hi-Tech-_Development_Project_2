8149757ded26cccc4568233707854cd1cec004c7
sfaiz801 <sfaiz777@rediffemail.com>
2024-06-21 16:22:45 +0530

    account number

---
+++ account.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+void accountnumber();
+
+void main()
+{
+    accountnumber();
+}
+
+void accountnumber()
+
+{
+    char accountNumber[20];
+
+    while (1)
+    {
+        printf("Please Enter 11 digit Account Number only: ");
+        scanf("%s", accountNumber);
+
+        if (strlen(accountNumber) == 11)
+        {
+            int isValid = 1;
+
+            for (int i = 0; i < 11; i++)
+            {
+                if (accountNumber[i] < '0' || accountNumber[i] > '9')
+                {
+                    isValid = 0;
+                    break;
+                }
+            }
+
+            if (isValid)
+            {
+                printf(" Account Number: %s\n", accountNumber);
+                break;
+            }
+            else
+            {
+                printf(" Invalid account number.Please try again. \n");
+            }
+        }
+        else
+        {
+            printf("Invalid account number.Please try again. \n");
+        }
+    }
+}
+
