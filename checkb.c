fb8edbf1dd1db7640e1772d6f6829068a536c494
Iamamit9 <akumarsingh330@gmail.com>
2024-06-24 16:20:33 +0530

    checkb

---
+++ checkb.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    checkbalance();
+    return 0;
+}
+void checkbalance()
+{
+    int balance;
+    balance = balance();
+    printf("Your current balance is : %d", balance);
+}
+
